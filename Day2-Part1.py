7e8c15cfeea0c4c89a5fe06a779d500ba0f59572
EZ3Z <30964083+EZ3Z@users.noreply.github.com>
2019-12-04 01:05:43 -0700

    arrays currently look at store at wrong indexes

--- Day2-Part1.py
+++ Day2-Part1.py
@@ -1,22 +1,50 @@
 # How many elements each list should have 
 n = 4 
-file=open(".\\Inputs\\inputDay2.txt", "r")
+file = open(".\\Inputs\\inputDay2.txt", "r")
 file = file.read()
 file = (file.split(','))
-#using list comprehension to seperate into chunks of 4 numbers
-myList = [file[i * n:(i + 1) * n] for i in range((len(file) + n - 1) // n )]  
+#converting string array to int aray
+for i in range(0, len(file)):
+    file[i] = int(file[i])
+print(file)
+myList = [file[i * n:(i + 1) * n] for i in range((len(file) + n - 1) // n )] 
+#using list comprehension to seperate into chunks of 4 numbers 
 #print(myList) 
-for element in myList:
-    elementHEAD = int(element[0])
-    print("\noperand is", elementHEAD)
-    if elementHEAD == 1:
-        print("we got 1")
-        for input in opcode:
-            if input[] == 1
-    if elementHEAD == 2:
-        print("we got 2")
-    if elementHEAD == 99:
-        print("we got 99")
-    elif elementHEAD != 99 and elementHEAD != 1 and elementHEAD != 2:
-        print("could not match correct operand for", elementHEAD)
+for opcode in myList:
+    #elementHEAD = int(opcode[0])
+    #print("\noperand is", elementHEAD)
+    if opcode[0] == 1:
+        #print("we got 1")
+        #inputIndex = 0
+        #for input in range(0,len(opcode) - 1):
+        #for input in opcode:
+        for index, input in enumerate(opcode):
+            print("\nopcode is", opcode)
+            print("index is", index)
+            print("input is", input)
+            #inputIndex += 1
+            if index == 1:
+                opcode[3] = opcode[1] + opcode[2]
+                print("opcode is now",opcode)
+    if opcode[0] == 2:
+        for index, input in enumerate(opcode):
+            print("\nopcode is", opcode)
+            print("index is", index)
+            print("input is", input)
+            #inputIndex += 1
+            if index == 1:
+                opcode[3] = opcode[1] * opcode[2]
+                print("opcode is now",opcode)
+        #print("we got 2")
+    if opcode[0] == 99:
+        for index, input in enumerate(opcode):
+            print("\nopcode is", opcode)
+            print("index is", index)
+            print("input is", input)
+            break
+        print("opcode is 99, stopping...")
+        break
+    elif opcode[0] != 99 and opcode[0] != 1 and opcode[0] != 2:
+        print("\ncould not match correct operand for", opcode[0])
+print(myList)
  
