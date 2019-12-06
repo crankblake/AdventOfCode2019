d29884a60d8e227e67affced2ded5aa23cb7bea6
EZ3Z <30964083+EZ3Z@users.noreply.github.com>
2019-12-05 21:19:33 -0700

    making progress

--- Day3-Part1.py
+++ Day3-Part1.py
@@ -1,3 +1,20 @@
-file = open(".\\Inputs\\inputDay3.txt", "r")
+import numpy as np
+file = open(".\\Inputs\\inputDay3TEST.txt", "r")
 wire1 = file.readlines(1)
 wire2 = file.readlines(2)
+wire1Array = wire1[0].split(',')
+wire2Array = wire2[0].split(',')
+array = np.ones([10,10], dtype=str)
+column = 0
+row = 9
+centralPort = row,column
+array[centralPort] = 'O' 
+np.set_printoptions(threshold=np.sys.maxsize)
+print(array)
+
+start = centralPort
+for command in wire1Array:
+    dir = command[0]
+    dist = command[1]
+    print(dir)
+    if dir == 'R':
