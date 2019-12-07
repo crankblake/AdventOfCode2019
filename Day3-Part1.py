fcb022079d1c1d959b365a82bff87133b80c9166
EZ3Z <crankblake@gmail.com>
2019-12-06 17:46:48 -0700

    can draw wires correctly!

--- Day3-Part1.py
+++ Day3-Part1.py
@@ -4,20 +4,81 @@ wire1 = file.readlines(1)
 wire2 = file.readlines(2)
 wire1Array = wire1[0].split(',')
 wire2Array = wire2[0].split(',')
-array = np.ones([10,10], dtype=str)
+array = np.empty([10,10], dtype=object)
+array.fill('__')
 column = 0
 row = 9
 centralPort = row,column
-array[centralPort] = 'O' 
+array[centralPort] = 'CP' 
 np.set_printoptions(threshold=np.sys.maxsize)
-print(array)
-
+#print(array)
 pointer = centralPort
-for command in wire1Array:
+for index, command in enumerate(wire1Array):
+    print("index is", index)
+    print("command is", command)
     dir = command[0]
     dist = int(command[1])
     print(dir)
     if dir == 'R':
-        for num in dist:
-            array[column + 1, row] = 'R'
-            dist -= 1
+        for i in range (0, dist):
+            #print("dist is", dist)
+            #print("i is", i)
+            array[row, column + 1] = 'R1'
+            #print("row, column", row,column)
+            column += 1
+            #print(column,row)
+            #print(array)
+    if dir == 'U':
+        for i in range (0, dist):
+            #print("dist is", dist)
+            #print("i is", i)
+            array[row - 1, column] = 'U1'
+            #print("row, column", row, column)
+            row -= 1
+            #print(column,row)
+            #print(array)
+    if dir == 'L':        
+        for i in range (0, dist):
+            array[row, column - 1] = 'L1'
+            column -= 1
+    if dir == 'D':
+        for i in range (0, dist):
+            array[row + 1, column] = 'D1'
+            row += 1 
+column = 0
+row = 9            
+for index, command in enumerate(wire2Array):
+    print("index is", index)
+    print("command is", command)
+    dir = command[0]
+    dist = int(command[1])
+    print(dir)
+    if dir == 'R':
+        for i in range (0, dist):
+            if array[row, column + 1] == 'U1' or array[row, column + 1] == 'D1':
+                array[row, column + 1] = 'XX'
+            else:
+                array[row, column + 1] = 'R2'
+            column += 1
+    if dir == 'U':
+        for i in range (0, dist):
+            if array[row - 1, column] == 'L1' or array[row - 1, column] == 'R1':
+                array[row - 1, column] = 'XX'
+            else:
+                array[row - 1, column] = 'U2'
+            row -= 1
+    if dir == 'L':        
+        for i in range (0, dist):
+            if array[row, column - 1] == 'U1' or array[row, column - 1] == 'D1':
+                array[row, column - 1] = 'XX'
+            else:
+                array[row, column - 1] = 'L2'
+            column -= 1
+    if dir == 'D':
+        for i in range (0, dist):
+            if array[row + 1, column] == 'L1' or  array[row + 1, column] == 'R1':
+                array[row + 1, column] = 'XX'
+            else:
+                array[row + 1, column] = 'D2'
+            row += 1               
+print(array)
